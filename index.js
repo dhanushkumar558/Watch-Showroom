b94ecfb2e93d8159895f495db440672d5f803cd4
dhanushkumar558 <dhanushkumar558@hotmail.com>
2024-04-11 17:15:30 +0530

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
         document.addEventListener('scroll',()=>
     {
         const header =document.querySelector('header');
-        if(scrollY>0)
+        if(scrollY>100)
         {
             header.classList.add('scrolled');
         }
@@ -10,17 +10,45 @@
         {
             header.classList.remove('scrolled');
         }
+ });
+   
 
 
 
 
 
+ const observer = new IntersectionObserver((entries) =>
+{
 
-
-
-
+    entries.forEach((entry) =>
+    {
+        console.log(entry)
+        if(entry.isIntersecting)
+        {
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
 
     });
+
+
+
+
+    
+
+
+
+
+    
+});
+
+
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
 
 
   
